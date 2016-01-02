007d07c059a9ddf068778914f89ea68b49fb37e8
Ivan Bodrov <ibodrov@jet.msk.su>
2016-01-03 01:55:44 +0300

    refactor a bit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate rustc_serialize;
+
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
 use std::thread;
@@ -6,8 +8,8 @@ use std::io::{BufReader, BufWriter};
 
 #[derive(Debug)]
 enum SmppError {
-    EOF,
-    UnsupportedCommandId,
+    InvalidMessageLength, // ESME_RINVMSGLEN
+    InvalidCommandId      // ESME_RINVCMDID
 }
 
 #[derive(Debug)]
@@ -67,10 +69,64 @@ enum SmppConnectionStatus {
     Bound
 }
 
-#[derive(Debug)]
 struct SmppConnection {
     status: SmppConnectionStatus,
     stream: TcpStream
+}
+
+impl SmppConnection {
+    fn new(stream: TcpStream) -> SmppConnection {
+        SmppConnection {
+            status: SmppConnectionStatus::NotYetBound,
+            stream: stream
+        }
+    }
+
+    fn read_pdu(r: &mut Iterator<Item=u8>) -> Result<SmppMessage, SmppError> {
+        let command_length = try!(read_u32(r));
+        let command = try!(SmppCommand::from_id(try!(read_u32(r)))
+                           .ok_or(SmppError::InvalidCommandId));
+        let command_status = try!(read_u32(r));
+        let sequence_number = try!(read_u32(r));
+        
+        let mut headers = HashMap::new();
+        match command {
+            SmppCommand::BindTransceiver => {
+                headers.insert("system_id", HeaderValue::Str(read_cstring(r)));
+                headers.insert("password", HeaderValue::Str(read_cstring(r)));
+                headers.insert("system_type", HeaderValue::Str(read_cstring(r)));
+                headers.insert("interface_version", HeaderValue::Byte(read_u8(r).unwrap()));
+                headers.insert("addr_ton", HeaderValue::Byte(read_u8(r).unwrap()));
+                headers.insert("addr_npi", HeaderValue::Byte(read_u8(r).unwrap()));
+                headers.insert("address_range", HeaderValue::Str(read_cstring(r)));
+            }
+            
+            _ => return Err(SmppError::InvalidCommandId)
+        };
+        
+        Ok(SmppMessage {
+            command_length: command_length,
+            command: command,
+            command_status: command_status,
+            sequence_number: sequence_number,
+            headers: headers
+        })
+    }
+
+    fn write_pdu(msg: SmppMessage, w: &mut Write) -> std::io::Result<usize> {
+        let mut buf: Vec<u8> = Vec::with_capacity(512);
+        let mut written = 0;
+        
+        try!(write_u32(&mut buf, msg.command.to_id().unwrap()));
+        try!(write_u32(&mut buf, msg.command_status));
+        try!(write_u32(&mut buf, msg.sequence_number));
+        try!(buf.write(&vec![0u8]));
+        
+        written = written + write_u32(w, (buf.len() + 4) as u32).unwrap();
+        written = written + w.write(&buf).unwrap();
+        
+        Ok(written)
+    }
 }
 
 fn read_u32(r: &mut Iterator<Item=u8>) -> Result<u32, SmppError> {
@@ -79,7 +135,7 @@ fn read_u32(r: &mut Iterator<Item=u8>) -> Result<u32, SmppError> {
     for _ in 0..4 {
         let v = match r.next() {
             Some(i) => i,
-            None => return Err(SmppError::EOF)
+            None => return Err(SmppError::InvalidMessageLength)
         };
 
         out = out << 4 | v as u32;
@@ -97,7 +153,7 @@ fn write_u32(w: &mut Write, i: u32) -> std::io::Result<usize> {
 fn read_u8(r: &mut Iterator<Item=u8>) -> Result<u8, SmppError> {
     match r.next() {
         Some(i) => Ok(i),
-        None => Err(SmppError::EOF)
+        None => Err(SmppError::InvalidMessageLength)
     }
 }
 
@@ -105,52 +161,6 @@ fn read_cstring(r: &mut Iterator<Item=u8>) -> String {
     r.take_while(|&c| c != 0)
         .map(|c| c as char)
         .collect::<String>()
-}
-
-fn read_pdu(r: &mut Iterator<Item=u8>) -> Result<SmppMessage, SmppError> {
-    let command_length = try!(read_u32(r));
-    let command = try!(SmppCommand::from_id(try!(read_u32(r)))
-                       .ok_or(SmppError::UnsupportedCommandId));
-    let command_status = try!(read_u32(r));
-    let sequence_number = try!(read_u32(r));
-
-    let mut headers = HashMap::new();
-    match command {
-        SmppCommand::BindTransceiver => {
-            headers.insert("system_id", HeaderValue::Str(read_cstring(r)));
-            headers.insert("password", HeaderValue::Str(read_cstring(r)));
-            headers.insert("system_type", HeaderValue::Str(read_cstring(r)));
-            headers.insert("interface_version", HeaderValue::Byte(read_u8(r).unwrap()));
-            headers.insert("addr_ton", HeaderValue::Byte(read_u8(r).unwrap()));
-            headers.insert("addr_npi", HeaderValue::Byte(read_u8(r).unwrap()));
-            headers.insert("address_range", HeaderValue::Str(read_cstring(r)));
-        }
-
-        _ => return Err(SmppError::UnsupportedCommandId)
-    };
-
-    Ok(SmppMessage {
-        command_length: command_length,
-        command: command,
-        command_status: command_status,
-        sequence_number: sequence_number,
-        headers: headers
-    })
-}
-
-fn write_pdu(msg: SmppMessage, w: &mut Write) -> std::io::Result<usize> {
-    let mut buf: Vec<u8> = Vec::with_capacity(512);
-    let mut written = 0;
-
-    try!(write_u32(&mut buf, msg.command.to_id().unwrap()));
-    try!(write_u32(&mut buf, msg.command_status));
-    try!(write_u32(&mut buf, msg.sequence_number));
-    try!(buf.write(&vec![0u8]));
-
-    written = written + write_u32(w, (buf.len() + 4) as u32).unwrap();
-    written = written + w.write(&buf).unwrap();
-
-    Ok(written)
 }
 
 fn handle_client(mut conn: SmppConnection) {
@@ -165,7 +175,7 @@ fn handle_client(mut conn: SmppConnection) {
     let mut writer = BufWriter::new(stream);
 
     loop {
-        let pdu = read_pdu(&mut bytes).unwrap();
+        let pdu = SmppConnection::read_pdu(&mut bytes).unwrap();
         println!("<< got: {:?}", pdu);
         
         match pdu.command {
@@ -186,7 +196,7 @@ fn handle_client(mut conn: SmppConnection) {
                     }
                 }
                 
-                write_pdu(resp, &mut writer).unwrap();
+                SmppConnection::write_pdu(resp, &mut writer).unwrap();
                 writer.flush().unwrap();
             }
 
@@ -201,10 +211,8 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let c = SmppConnection{ status: SmppConnectionStatus::NotYetBound,
-                                        stream: stream };
-                
                 thread::spawn(move || {
+                    let c = SmppConnection::new(stream);
                     handle_client(c);
                 });
             }
