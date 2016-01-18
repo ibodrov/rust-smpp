7b3a880e000893a72c0d4bf8db998d508f099fe8
Ivan Bodrov <ibodrov@jet.msk.su>
2016-01-19 00:56:05 +0300

    move utility fns into the separate module

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-mod smpp;
-
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
 use std::thread;
 use std::collections::HashMap;
 use std::io::{BufReader, BufWriter};
 
-use smpp::{SmppCommandStatus, SmppCommand, HeaderValue, SmppMessage};
+mod smpp;
+mod util;
 
+use smpp::{SmppCommandStatus, SmppCommand, HeaderValue, SmppMessage};
+use util::{read_u32, write_u32, read_u8, read_cstring, read_exact};
 
 #[derive(Debug)]
 enum SmppConnectionStatus {
@@ -136,44 +137,6 @@ impl SmppConnection {
 
         Ok(written)
     }
-}
-
-/// Reads an u32 value from the specified iterator.
-fn read_u32(r: &mut Iterator<Item=u8>) -> Result<u32, SmppCommandStatus> {
-    let mut out = 0;
-
-    for _ in 0..4 {
-        let v = match r.next() {
-            Some(i) => i,
-            None => return Err(SmppCommandStatus::InvalidMessageLength)
-        };
-
-        out = out << 4 | v as u32;
-    }
-
-    Ok(out)
-}
-
-fn write_u32(w: &mut Write, i: u32) -> std::io::Result<usize> {
-    let ab = unsafe { std::mem::transmute::<u32, [u8; 4]>(i.to_be()) };
-    w.write(&ab)
-}
-
-fn read_u8(r: &mut Iterator<Item=u8>) -> Result<u8, SmppCommandStatus> {
-    match r.next() {
-        Some(i) => Ok(i),
-        None => Err(SmppCommandStatus::InvalidMessageLength)
-    }
-}
-
-fn read_cstring(r: &mut Iterator<Item=u8>) -> String {
-    r.take_while(|&c| c != 0)
-        .map(|c| c as char)
-        .collect::<String>()
-}
-
-fn read_exact(r: &mut Iterator<Item=u8>, len: usize) -> Vec<u8> {
-    r.take(len).collect()
 }
 
 fn handle_client(mut conn: SmppConnection) {
